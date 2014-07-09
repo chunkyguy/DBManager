524abd852bed2ab18e9b92cc9293a3fb321e8cdc
Sidharth Juyal <sid@whackylabs.com>
2014-07-10 04:20:20 +0530

    Fix indentation

--- DBManager/src/DBManager.cpp
+++ DBManager/src/DBManager.cpp
@@ -13,7 +13,7 @@
 #include <sqlite3.h>
 
 namespace database {
-
+    
 #pragma mark - private functions
     namespace {
         int callback(void *result, int rowNo, char **rows, char **cols) {
@@ -26,16 +26,6 @@ namespace database {
             std::vector<std::map<std::string, std::string> > *r = static_cast<std::vector<std::map<std::string, std::string> >*>(result);
             r->push_back(item);
             
-            //	std::vector<> resultArr;
-            //	for(int i = 0; i < rowNo; ++i)	{
-            //		std::map<> objectKeyDict;
-            //		objectKeyDict = [NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithUTF8String:cols[i]],@"column",[NSString stringWithUTF8String:rows[i]],@"rows",nil];
-            //		[resultArr addObject:objectKeyDict];
-            //	}
-            //	[sqlPool release];
-            //	//NSLog(@"callback: %@",resultArr);
-            //	[queryResult addObject:resultArr];
-            //	//[];
             return 0;
         }
         
@@ -46,31 +36,28 @@ namespace database {
             return (sqlite3_exec(slf.object(), query.c_str(), callback, result, 0) == SQLITE_OK);
         }
     }
-
+    
 #pragma mark - Object
-    Object::Object(const std::string &path)
-    {
-		bool res = (sqlite3_open(path.c_str(), &_object) == SQLITE_OK);
-		assert(res);
+    Object::Object(const std::string &path) {
+        bool res = (sqlite3_open(path.c_str(), &_object) == SQLITE_OK);
+        assert(res);
 	}
 	
-	Object::~Object()
-    {
-		sqlite3_close(_object);
+	Object::~Object() {
+        sqlite3_close(_object);
 	}
-
+    
 #pragma mark - Query
 	Query::Query(const Object &database, const std::string &statement) :
 	result_(),
-	success_(true)
-	{
-		// Execute
-		if (!Execute(database, statement, &result_)) {
-			std::cout << "Query failed" << std::endl;
-			success_ = false;
-		}
-		
-		seek_ = result_.begin();
+	success_(true) {
+        // Execute
+        if (!Execute(database, statement, &result_)) {
+            std::cout << "Query failed" << std::endl;
+            success_ = false;
+        }
+        
+        seek_ = result_.begin();
 	}
 	
 	bool Query::End() const {
@@ -78,7 +65,7 @@ namespace database {
 	}
 	
 	std::map<std::string, std::string> Query::Result() const {
-		return *seek_;
+        return *seek_;
 	}
 	
 	void Query::Next() {
